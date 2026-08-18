5732306552dfb742de2bd25292cc4e86154fc804
satriazee <zeesatria098@gmail.com>
2026-08-18 22:52:20 +0700

    Update egg.lua

--- egg.lua
+++ egg.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+loadstring(game:HttpGet("https://muihub.dev/s/e0d9a52ae55b201222ac08518f51c29b"))()
